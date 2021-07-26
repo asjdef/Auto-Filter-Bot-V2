3159b7ce3b44ae42051759c858e58689990b4e0b
asjdef <87009167+asjdef@users.noreply.github.com>
2021-07-26 10:31:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "🔰 JOIN WITH US 🔰", url="https://t.me/SL_MEDIA_TECH_GRUOP")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "✨ SUPPORT 🤖", url="https://t.me/SL_MEDIA_TECH_GRUOP")
                     ]
                 ]
             ),
@@ -68,7 +68,7 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")
+                            "✨ Bot Owner 🇱🇰", url="https://t.me/Akilawiduruwan")
                     ]
                 ]
             ),
