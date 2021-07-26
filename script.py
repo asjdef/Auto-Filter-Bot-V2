6338782906f28a0866883c54b3b59745dfc9bfeb
asjdef <87009167+asjdef@users.noreply.github.com>
2021-07-26 10:28:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -53,17 +53,17 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @TroJanzHEX</b>"""
+<b>© @SL_MEDIA_TECH_GRUOP</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Unlimited Filter Bot 🇱🇰 V2</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Akilawiduruwan
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>Our Main Group :</b> @SL_MEDIA_TECH_GRUOP
 
 """
